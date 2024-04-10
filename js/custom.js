302c616e3398a0e7114524945858699cf136b698
Sajid Haider <165992305+sajid-haider@users.noreply.github.com>
2024-04-10 12:53:41 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+// slideer
+$(function(){
+  $('.slider-section-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1,
+        },
+        1200:{
+            items:1,
+        }
+    }
+})
+})
+// click menu
